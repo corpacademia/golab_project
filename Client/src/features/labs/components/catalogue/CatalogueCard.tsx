5d830188cb59692ad311d8417ecc2b07d38bc0b5
Parveez Khan <parveezkhan9611@gmail.com>
2025-01-06 13:37:49 +0530

    changes

--- Client/src/features/labs/components/catalogue/CatalogueCard.tsx
+++ Client/src/features/labs/components/catalogue/CatalogueCard.tsx
@@ -30,13 +30,17 @@ export const CatalogueCard: React.FC<CatalogueCardProps> = ({ lab }) => {
 
         {/* Technologies */}
         <div className="flex flex-wrap gap-2 mb-3">
-          {lab.technologies.slice(0, 3).map(tech => (
+          {/* {lab.technologies.slice(0, 3).map(tech => (
             <span key={tech} 
               className="px-2 py-1 text-xs font-medium bg-dark-300/50 
                        text-primary-300 rounded-full">
               {tech}
             </span>
-          ))}
+          ))} */}
+          <span  
+              className="px-2 py-1 text-xs font-medium bg-dark-300/50 
+                       text-primary-300 rounded-full">
+            </span>
         </div>
 
         {/* Metrics */}
